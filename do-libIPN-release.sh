0306be5283fd7ea04656f9ac2bd29da007118c71
Plamen K. Kosseff <p.kosseff@gmail.com>
2023-05-21 21:39:53 +0300

    Releasing 3.0.1

--- do-libIPN-release.sh
+++ do-libIPN-release.sh
@@ -3,4 +3,19 @@
 . ~/.config/secrets/modrinth.sh
 . ~/.config/secrets/curseforge.sh
 
-./gradlew --max-workers 2 clean build modrinth curseforge
+
+pushd .
+
+cd $(mktemp -d /tmp/libIPN-release.XXXX)
+
+git clone git@github.com:blackd/libIPN.git libIPN
+
+cd libIPN/description
+
+python build_release_notes.py
+
+cd ..
+
+./gradlew --max-workers 32 clean compileKotlin compileJava
+
+./gradlew --max-workers 4 build modrinth curseforge
