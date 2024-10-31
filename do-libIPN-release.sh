c026661a6276a8fdd3a2c456f02f9d280a710a79
Plamen K. Kosseff <p.kosseff@gmail.com>
2024-10-31 15:42:39 +0200

    Releasing v6.1.0

--- do-libIPN-release.sh
+++ do-libIPN-release.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+#
+# Inventory Profiles Next
+#
+#   Copyright (c) 2024 Plamen K. Kosseff <p.kosseff@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 if [[ n$IPNEXT_RELEASE != "n" ]]; then
   . ~/.config/secrets/modrinth.sh
   . ~/.config/secrets/curseforge.sh
 fi
 
-
 pushd .
+
 mkdir /tmp/IPN
 cd $(mktemp -d /tmp/IPN/libIPN-release.XXXX)
 
@@ -25,21 +44,21 @@ fi
 
 cd libIPN/description
 
+
 python build_release_notes.py
 
 cd ..
 
-
 if [[ n$IPNEXT_PATREON != "n" ]]; then
-  IPNEXT_RELEASE=1 --max-workers 32 clean compileKotlin compileJava
+  IPNEXT_RELEASE=1 ./gradlew --max-workers 32 clean compileKotlin compileJava
 else
   ./gradlew --max-workers 32 clean compileKotlin compileJava
 fi
 
-GRADLE_ARG="build"
+GRADLE_ARG="--exclude-task compileTestJava --exclude-task test build"
 
 if [[ n$IPNEXT_RELEASE != "n" ]]; then
-  GRADLE_ARG="--max-workers 4 $GRADLE_ARG publishAllPublicationsToIpnOfficialRepoRepository modrinth curseforge"
+  GRADLE_ARG="--max-workers 32 $GRADLE_ARG publishAllPublicationsToIpnOfficialRepoRepository modrinth curseforge"
   ./gradlew $GRADLE_ARG
 elif [[ n$IPNEXT_PATREON != "n" ]]; then
   GRADLE_ARG="--max-workers 32 $GRADLE_ARG"
@@ -49,6 +68,8 @@ else
   ./gradlew $GRADLE_ARG
 fi
 
+
+ls -la build/lib/
 
 pwd
 
